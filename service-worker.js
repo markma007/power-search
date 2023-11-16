f6347f3a1b03f066843e7a91d3475ab7b1543a41
markma007 <yunzhi7@yahoo.com>
2023-11-16 16:21:12 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
+// service-worker.js
 
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+async function onStartWindow() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions); 
+    //console.log(`Command: ${command} from ${tab.windowId},${tab.index}`);
+	const response = await chrome.tabs.sendMessage(tab.id, {command: "start-window"});
+	//console.log(response);
 }
 
 chrome.commands.onCommand.addListener(async (command) => {
-  if (command==='run-foo') {  
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions); 
-    console.log(`Command: ${command} from ${tab.windowId},${tab.index}`);
+  if (command==='start-window') {  
+    onStartWindow()
   }
 });
